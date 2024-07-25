bacb099884976f3acd7d0c419e4faa1d47fb10b6
AlexsaFox <83453022+AlexsaFox@users.noreply.github.com>
2024-07-25 20:28:58 +0300

    add base index page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from main.models import Dog
+
+
 # Create your views here.
+
+def index_page(request):
+    context = {
+        'dogs': Dog.get_all_dogs(),
+    }
+    return render(request, 'index.html', context)
+
